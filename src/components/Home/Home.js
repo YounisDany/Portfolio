eeee104a96da2ecb64f764ff9fd54b8228fa0d8e
Younis Dany | يونس ضاعني <111737417+YounisDany@users.noreply.github.com>
2023-08-14 09:38:45 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,17 +15,17 @@ function Home() {
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
+                <span translate="no" className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Younis Dany</strong>
+                <strong translate="no" className="main-name"> Younis Dany</strong>
               </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
+              <div translate="no" style={{ padding: 50, textAlign: "left" }}>
                 <Type/>
               </div>
             </Col>
